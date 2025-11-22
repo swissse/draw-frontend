c93fc8211b4079f40be019accf29ae95a4610262
maxim linkov <linkovmax17@gmail.com>
2025-11-22 21:58:14 +0300

    upd

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api" // порт твоего backend'а
+  baseURL: "https://draw-backend-5x3l.onrender.com/api"
 });
 
 export default api;
